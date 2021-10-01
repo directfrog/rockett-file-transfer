991ffe7b37c42343ee8363ff47170d295d5126b9
directfrog <directfrog@gmail.com>
2021-10-01 11:54:21 +0100

    Delete Rockett directory

--- Rockett/responses.py
+++
@@ -1,7 +0,0 @@
-##### This file is really just a work in progress #####
-# This will be used for simple greetings and also for tracking voice commands
-
-responses = {
-	['hi', 'hello', 'how are you doing', 'good morning', 'good evening']:['hello, my name is rockett', 'hi, i"m rocket'],#basic greeting
-	['functions', 'what can you do']:['to find a recipe, just say: find recipes and to open your recipes file, just say: open recipes']
-}
